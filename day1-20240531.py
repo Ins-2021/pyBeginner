db4e51714b79b3a4140e468e84813125aee106f5
赵显华 <zhaoxianhua@chinahuanong.com.cn>
2024-06-03 16:45:21 +0800

    py第一天

---
+++ day1-20240531.py
@@ -0,0 +1,13 @@
+print('hello, world',"i'm zxh")
+print(3000)
+print(100+200)
+print('100+200=',100+200)
+#name=input()
+#print('hello',name)
+print('I\'m ok.')
+print('I\'m learning\nPython.')
+print('''line1
+line2
+line3''')
+print(r'''hello\n
+      world''')
